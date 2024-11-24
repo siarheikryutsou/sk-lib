3a182ad481033121b0b0476e8acb09d415c10513
Sergey Krivtsov <flashgangsta@gmail.com>
2024-11-24 18:28:15 +0100

    Updated random generation

--- utils/getRandomNumberInRange.ts
+++ utils/getRandomNumberInRange.ts
@@ -1,7 +1,8 @@
-export function getRandomNumberInRange(min: number, max: number): number {
+export function getRandomNumberInRange(min: number, max: number, roundMethod?: (value: number) => number): number {
     if (min > max) {
         throw new Error("min must be less than or equal to max");
     }
 
-    return Math.random() * (max - min) + min;
+    const result = Math.random() * (max - min) + min;
+    return typeof roundMethod === "function" ? roundMethod(result) : result;
 }

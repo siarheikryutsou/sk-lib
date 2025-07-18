37fd1be4e8c4ce80f0c197aec416cbf1a3381cbe
Sergey Krivtsov <flashgangsta@gmail.com>
2025-07-18 16:04:19 +0200

    validateEmail updates

--- validators/validateEmail.ts
+++ validators/validateEmail.ts
@@ -1,15 +1,14 @@
 /**
+ * Regular expression for validating email addresses.
+ */
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+/**
  * Validates if a given string is a valid email address.
- *
- * The function uses a basic regular expression to check if the input string
- * follows the general structure of an email: `something@something.something`.
  *
  * @param {string} email - The email address to validate.
  * @returns {boolean} `true` if the email is valid, otherwise `false`.
  */
-
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 export function validateEmail(email: string): boolean {
     return emailRegex.test(email);
 }
